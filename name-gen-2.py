cf5a956eee2f06b9d15c7e63c67b867cdff4becb
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-04-07 09:51:32 -0500

    adding py2

---
+++ name-gen-2.py
@@ -0,0 +1,13 @@
+import random, string
+
+def generator():
+    letter1 = random.choice(string.ascii_lowercase)
+    letter2 = random.choice(string.ascii_lowercase)
+    letter3 = random.choice(string.ascii_lowercase)
+    letter4 = random.choice(string.ascii_lowercase)
+    letter5 = random.choice(string.ascii_lowercase)
+
+    name = letter1+letter2+letter3+letter4+letter5
+    return(name)
+
+    
--- name-gen.py
+++ name-gen.py
@@ -1,3 +1,7 @@
 import string
 print(string.ascii_letters)
 print(string.ascii_lowercase)
+
+import random
+print(random.choice('Pull Letter From Here'))
+print(random.choice(string.ascii_lowercase))
